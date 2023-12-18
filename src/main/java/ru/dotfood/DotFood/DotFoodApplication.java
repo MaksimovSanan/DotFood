7cab094ceee3ff46ba80b54c4fa2a531004eb8c5
songrash <00-90->
2023-12-18 22:44:00 +0300

    sql

---
+++ src/main/java/ru/dotfood/DotFood/DotFoodApplication.java
@@ -0,0 +1,13 @@
+package ru.dotfood.DotFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotFoodApplication.class, args);
+	}
+
+}
